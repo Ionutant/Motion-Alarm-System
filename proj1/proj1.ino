0c9ff61b0e54681d15d16f836bfe0e1b294dbc1f
Ionutant <ionut.antofi@yahoo.com>
2024-01-15 21:53:56 +0200

    m

--- proj1/proj1.ino
+++ proj1/proj1.ino
@@ -1,125 +1,163 @@
+#define BLYNK_TEMPLATE_ID "TMPL4o7ybbAC3"
+#define BLYNK_TEMPLATE_NAME "Motion Alarm"
+#define BLYNK_AUTH_TOKEN "hxxF1suN-YimVTyUyMGaRO7RluO7wKfs"
+
+#include <Arduino.h>
 #include <WiFi.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include <BlynkSimpleEsp32.h>
 
-const char* ssid     = "John";
-const char* password = "ionutu24";
+char auth[] = "hxxF1suN-YimVTyUyMGaRO7RluO7wKfs";
+char ssid[] = "iPhone";
+char pass[] = "123456789";
+const int sensorPin = 2;
+const int buzzerPin = 5; 
+const int buzzerFrequency = 800; 
 
-int btnGPIO = 0;
-int vibrationSensorPin = 2; // Exemplu: D2 pe ESP32
-int btnState = false;
-bool motionDetected = false;
+TaskHandle_t buzzerTask;
+TaskHandle_t wifiTask;
+TaskHandle_t notificationTask;
+TaskHandle_t vibrationTask;
+SemaphoreHandle_t xMotionSemaphore; 
 
-void connectToWiFi(void *parameter) {
-    Serial.println();
-    Serial.print("[WiFi] Connecting to ");
-    Serial.println(ssid);
 
-    WiFi.begin(ssid, password);
-
-    while (true) {
-        switch(WiFi.status()) {
-            case WL_CONNECTED:
-                Serial.println("[WiFi] WiFi is connected!");
-                Serial.print("[WiFi] IP address: ");
-                Serial.println(WiFi.localIP());
-                vTaskDelete(NULL);
-                break;
-            case WL_NO_SSID_AVAIL:
-                Serial.println("[WiFi] SSID not found");
-                break;
-            case WL_CONNECT_FAILED:
-                Serial.print("[WiFi] Failed - WiFi not connected! Reason: ");
-                break;
-            case WL_CONNECTION_LOST:
-                Serial.println("[WiFi] Connection was lost");
-                break;
-            case WL_SCAN_COMPLETED:
-                Serial.println("[WiFi] Scan is completed");
-                break;
-            case WL_DISCONNECTED:
-                Serial.println("[WiFi] WiFi is disconnected");
-                break;
-            default:
-                Serial.print("[WiFi] WiFi Status: ");
-                Serial.println(WiFi.status());
-                break;
-        }
-        vTaskDelay(pdMS_TO_TICKS(500)); // Pauza de 500 de milisecunde
-    }
-}
-
-void readVibrationSensor(void *parameter) {
-    while (true) {
-        // Citeste starea senzorului de vibratii
-        motionDetected = digitalRead(vibrationSensorPin) == HIGH;
-        vTaskDelay(pdMS_TO_TICKS(100)); // Pauza de 100 de milisecunde
-    }
-}
-
-void sendNotification(void *parameter) {
-    while (true) {
-        if (motionDetected) {
-            // Trimite notificare la Serial Monitor
-            Serial.println("[Motion] Motion detected! ");
-
-            // Așteaptă o perioadă de timp pentru a evita trimiterea continuă a notificărilor
-            vTaskDelay(pdMS_TO_TICKS(5000)); // Pauza de 5 secunde
-        } else {
-            // Așteaptă un scurt interval pentru a economisi resursele CPU
-            vTaskDelay(pdMS_TO_TICKS(100)); // Pauza de 100 de milisecunde
-        }
-    }
-}
+void activateBuzzer(void *pvParameters);
+void connectToWiFi(void *pvParameters);
+void readVibrationSensor(void *pvParameters);
+void sendNotification(void *pvParameters);
 
 void setup() {
-    Serial.begin(115200);
-    delay(10);
-
-    // Set GPIO0 Boot button și pinul senzorului de vibrații ca intrări
-    pinMode(btnGPIO, INPUT);
-    pinMode(vibrationSensorPin, INPUT);
-
-    xTaskCreatePinnedToCore(
-        connectToWiFi,    // Funcția task-ului
-        "connectToWiFi",  // Numele task-ului
-        8192,             // Stiva task-ului (dimensiunea în octeți)
-        NULL,             // Parametrul task-ului
-        1,                // Prioritatea task-ului
-        NULL,             // Handle-ul task-ului 
-        1                 // Nucleul procesorului pe care rulează task-ul
+  Serial.begin(115200);
+  
+ 
+ 
+  xTaskCreatePinnedToCore(
+        connectToWiFi,
+        "wifiTask",
+        10000,
+        NULL,
+        4,
+        &wifiTask,
+        1
     );
 
-    xTaskCreatePinnedToCore(
-        readVibrationSensor,   // Funcția task-ului
-        "readVibrationSensor", // Numele task-ului
-        4096,                  // Stiva task-ului (dimensiunea în octeți)
-        NULL,                  // Parametrul task-ului 
-        1,                     // Prioritatea task-ului
-        NULL,                  // Handle-ul task-ului 
-        1                      // Nucleul procesorului pe care rulează task-ul
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi!");
+
+  Blynk.begin(auth, ssid, pass);
+  while (Blynk.connected() == false) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("Connected to Blynk!");
+
+  xMotionSemaphore = xSemaphoreCreateBinary();
+
+  pinMode(sensorPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+ 
+  xTaskCreatePinnedToCore(
+        activateBuzzer,
+        "buzzerTask",
+        10000,
+        NULL,
+        2,
+        &buzzerTask,
+        1
     );
 
-    xTaskCreatePinnedToCore(
-        sendNotification,   // Funcția task-ului
-        "sendNotification", // Numele task-ului
-        4096,                // Stiva task-ului (dimensiunea în octeți)
-        NULL,                // Parametrul task-ului 
-        1,                   // Prioritatea task-ului
-        NULL,                // Handle-ul task-ului 
-        1                    // Nucleul procesorului pe care rulează task-ul
+ 
+  xTaskCreatePinnedToCore(
+        readVibrationSensor,
+        "vibrationTask",
+        4096,
+        NULL,
+        3,
+        &vibrationTask,
+        1
+    );
+
+
+  xTaskCreatePinnedToCore(
+        sendNotification,
+        "notificationTask",
+        8192,
+        NULL,
+        2,
+        &notificationTask,
+        1
     );
 }
 
 void loop() {
-   
-    btnState = digitalRead(btnGPIO);
+
+}
+
+void activateBuzzer(void *pvParameters) {
+  while (1) {
+    int motionState = digitalRead(sensorPin);
+
+    if (motionState == HIGH) {
+      tone(buzzerPin, buzzerFrequency);
+      delay(1000); 
+      noTone(buzzerPin);
+    }
+
+    vTaskDelay(100 / portTICK_PERIOD_MS); 
+  }
+}
+
+void connectToWiFi(void *pvParameters) {
+    Serial.println("[WiFi] Connecting to WiFi...");
     
-    if (btnState == LOW) {
-        Serial.println("[WiFi] Disconnecting from WiFi!");
-        if(WiFi.disconnect(true, false)){
-            Serial.println("[WiFi] Disconnected from WiFi!");
-        }
+    WiFi.begin(ssid, pass);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
         delay(1000);
+    }
+
+    Serial.println();
+    Serial.println("[WiFi] Connected to WiFi");
+     vTaskDelete(NULL);
+}
+
+
+void readVibrationSensor(void *pvParameters) {
+  int vibrationlast = 0;
+    while (1) {
+        
+        int sensorValue = digitalRead(sensorPin);
+       
+     
+   if(sensorValue == HIGH && vibrationlast==0){
+        xSemaphoreGive(xMotionSemaphore);
+   }
+   vibrationlast= sensorValue;
+        Serial.println("Vibration Sensor Value: " + String(sensorValue));
+  //       Serial.print("Valoarea portTICK_PERIOD_MS este: ");
+  // Serial.println(portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(300)); 
+    }
+}
+
+void sendNotification(void *pvParameters) {
+    while (1) {
+        if (Blynk.connected()) {
+            xSemaphoreTake(xMotionSemaphore, portMAX_DELAY);
+           
+                Serial.println("Sending Notification...");
+                Blynk.logEvent("m_alarm");
+               
+            vTaskDelay(pdMS_TO_TICKS(100));
+        } else {
+            Serial.println("Blynk not connected. Reconnecting...");
+            Blynk.connect();
+            vTaskDelay(pdMS_TO_TICKS(5000)); 
+        }
     }
 }
